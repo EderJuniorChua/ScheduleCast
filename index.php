b9b28e9df4527c6fda85f464334a2e00ac8a1442
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-09-03 23:50:45 +0800

    Changed web page title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <html class="h-full">
 
 <head>
-    <title>Login Page</title>
+    <title>CIITzen Authentify</title>
     <link rel="stylesheet" href="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
